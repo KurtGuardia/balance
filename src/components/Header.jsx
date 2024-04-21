02e292f2ff29e251cf444ab64f370d5463371291
Kurt Guardia <kaguardiagamarra@wideagency.com>
2024-04-21 14:49:59 +0200

    Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { ReactComponent as Logo } from '../imgs/logo.svg'
 import '../styles/header.scss'
 
 export default function Header() {
   return (
     <div className='header'>
+      <div className='header_logo'>
+        <Logo />
+        <p className='header_logo-name'>Balance</p>
+      </div>
       <ul>
         <li>Sobre Mí</li>
         <li>Servicios</li>
