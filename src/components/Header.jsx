d4c035180af251b6498c826a1a3bcfd64e9dae0d
Kurt Guardia <kurtguardia@gmail.com>
2024-04-28 15:21:12 +0200

    ham nav done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,30 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 export default function Header() {
   const [isSticky, setIsSticky] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [isSmallScreen, setIsSmallScreen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  const list = [
+    ['Sobre Mí', 'me'],
+    ['Servicios', 'services'],
+    ['Proceso', 'process'],
+    ['Contacto', 'contact'],
+  ]
 
   useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.screen.width
+      if (screenWidth <= 768) {
+        setIsSmallScreen(true)
+      } else {
+        setIsSmallScreen(false)
+      }
+    }
+
     const handleScroll = () => {
       if (window.scrollY > 800) {
         setIsSticky(true)
@@ -16,48 +38,76 @@ export default function Header() {
       }
     }
 
+    window.addEventListener('resize', handleResize)
+    handleResize()
     window.addEventListener('scroll', handleScroll)
 
     return () => {
       window.removeEventListener('scroll', handleScroll)
+      window.removeEventListener('resize', handleResize)
     }
   }, [])
 
   return (
-    <div className={isSticky ? 'header sticky' : 'header'}>
-      <AnchorLink href='#top' offset='150'>
-        <div className='header_logo'>
-          <Logo />
-          <p className='header_logo-name'>Balance</p>
+    <>
+      {isSmallScreen && (
+        <div
+          className={`ham ${menuOpen ? 'active' : ''}`}
+          onClick={() => toggleMenu()}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-      </AnchorLink>
-      <ul>
-        {[
-          ['Sobre Mí', 'me'],
-          ['Servicios', 'services'],
-          ['Proceso', 'process'],
-          ['Contacto', 'contact'],
-        ].map((item) => (
-          <motion.li
-            key={item}
-            initial={{
-              borderBottom: '2px solid transparent',
-            }}
-            whileHover={{
-              scale: 1.15,
-              borderBottom: '2px solid #F6F6EA',
-              transition: {
-                duration: 0.5,
-                ease: 'easeInOut',
-              },
-            }}
-          >
-            <AnchorLink href={`#${item[1]}`}>
-              {item[0]}
-            </AnchorLink>
-          </motion.li>
-        ))}
-      </ul>
-    </div>
+      )}
+      {(!isSmallScreen || menuOpen) && (
+        <div
+          className={
+            isSticky && !isSmallScreen
+              ? 'header sticky'
+              : 'header'
+          }
+        >
+          <AnchorLink href='#top' offset='150'>
+            <div
+              className='header_logo'
+              onClick={() => toggleMenu()}
+            >
+              <Logo />
+              {!isSmallScreen && (
+                <p className='header_logo-name'>Balance</p>
+              )}
+            </div>
+          </AnchorLink>
+
+          <ul>
+            {list.map((item) => (
+              <motion.li
+                key={item}
+                initial={{
+                  borderBottom: '2px solid transparent',
+                }}
+                whileHover={{
+                  scale: 1.15,
+                  borderBottom: '2px solid #F6F6EA',
+                  transition: {
+                    duration: 0.5,
+                    ease: 'easeInOut',
+                  },
+                }}
+                onClick={() => toggleMenu()}
+              >
+                <AnchorLink
+                  href={`#${item[1]}`}
+                  offset='150'
+                >
+                  {item[0]}
+                </AnchorLink>
+              </motion.li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   )
 }
