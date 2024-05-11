6bd502c6d1ff747e94b61523c3a498e46d1e876f
Kurt Guardia <kurtguardia@gmail.com>
2024-05-11 21:56:26 +0200

    Fixed errors

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,36 +1,36 @@
 import React, { useState } from 'react'
 import '../styles/process.scss'
 import { motion } from 'framer-motion'
-import { ReactComponent as Dropdown } from '../imgs/dropdown.svg'
+// import { ReactComponent as Dropdown } from '../imgs/dropdown.svg'
 
 export default function Process() {
-  const [openStep, setOpenStep] = useState({})
+  // const [openStep, setOpenStep] = useState({})
 
-  const steps = [
-    {
-      title: 'Primera Consulta:',
-      text: 'Llegará a tu correo un enlace para conectarnos por Videollamada, la cual tendrá una duración de 45 minutos a una hora, donde evaluaremos tus hábitos alimentarios, estado emocional-mental, estilo de vida, tus gustos y preferencias entre otras cosas. También revisaremos objetivos en base a tu caso.',
-    },
-    {
-      title: 'Desarrollo de tu plan personalizado:',
-      text: 'De acuerdo a tus necesidades elaboraré tu plan alimentario con recomendaciones de estilo de vida y recetas, incluiré técnicas de relajación y manejo de estrés además de suplementación si es necesaria.',
-    },
-    {
-      title: 'Entrega del Plan:',
-      text: 'Una semana después de la primera consulta, tendremos una nueva videollamada donde te compartiré tu plan alimentario con las recomendaciones mencionadas. Después te enviaré todo el material a tu correo electrónico. Cualquier duda que tengas puedes escribirme al correo: balancenutricionintegrativa@gmail.com',
-    },
-    {
-      title: 'Consulta de Seguimiento:',
-      text: 'Dos semanas después evaluaremos como te sientes, tu adherencia al plan alimentario y a las recomendaciones de estilo de vida, si es necesario haremos modificaciones.',
-    },
-  ]
+  // const steps = [
+  //   {
+  //     title: 'Primera Consulta:',
+  //     text: 'Llegará a tu correo un enlace para conectarnos por Videollamada, la cual tendrá una duración de 45 minutos a una hora, donde evaluaremos tus hábitos alimentarios, estado emocional-mental, estilo de vida, tus gustos y preferencias entre otras cosas. También revisaremos objetivos en base a tu caso.',
+  //   },
+  //   {
+  //     title: 'Desarrollo de tu plan personalizado:',
+  //     text: 'De acuerdo a tus necesidades elaboraré tu plan alimentario con recomendaciones de estilo de vida y recetas, incluiré técnicas de relajación y manejo de estrés además de suplementación si es necesaria.',
+  //   },
+  //   {
+  //     title: 'Entrega del Plan:',
+  //     text: 'Una semana después de la primera consulta, tendremos una nueva videollamada donde te compartiré tu plan alimentario con las recomendaciones mencionadas. Después te enviaré todo el material a tu correo electrónico. Cualquier duda que tengas puedes escribirme al correo: balancenutricionintegrativa@gmail.com',
+  //   },
+  //   {
+  //     title: 'Consulta de Seguimiento:',
+  //     text: 'Dos semanas después evaluaremos como te sientes, tu adherencia al plan alimentario y a las recomendaciones de estilo de vida, si es necesario haremos modificaciones.',
+  //   },
+  // ]
 
-  const toggleStep = (index) => {
-    setOpenStep((prevState) => ({
-      ...prevState,
-      [index]: !prevState[index],
-    }))
-  }
+  // const toggleStep = (index) => {
+  //   setOpenStep((prevState) => ({
+  //     ...prevState,
+  //     [index]: !prevState[index],
+  //   }))
+  // }
 
   return (
     <div className='process' id='process'>
