fce4d850df2d699cc67875d4cfbe55cce8a7460c
Kurt Guardia <kurtguardia@gmail.com>
2024-08-11 15:51:22 +0200

    done with client's request on the Me section

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -4,7 +4,6 @@ import PNIE from '../imgs/PNIE.webp'
 import microbiota from '../imgs/microbiota.webp'
 import { useState } from 'react'
 import { motion } from 'framer-motion'
-// import { ReactComponent as Dropdown } from '../imgs/dropdown.svg'
 
 export default function Me() {
   const [pnieOpen, setPnieOpen] = useState(false)
@@ -49,94 +48,92 @@ export default function Me() {
         tanto física como mental. Siempre me ha gustado
         entregar una atención personalizada de acuerdo con
         las características de cada paciente, sin embargo,
-        sentía que faltaban piezas fundamentales para un{' '}
-        <b>entendimiento más profundo de la salud;</b> es
+        sentía que faltaban piezas fundamentales para un
+        <b> entendimiento más profundo de la salud; </b> es
         así como decidí formarme en dos apasionantes áreas,
-        la{' '}
+        la
+        <b> psico-neuro-inmuno-endocrinología</b>
         <span onClick={() => setPnieOpen(!pnieOpen)}>
           {' '}
-          psico-neuro-inmuno-endocrinología
-        </span>{' '}
-        y la{' '}
+          (ver qué es){' '}
+        </span>
+        y la <b> microbiota intestinal </b>
         <span
           onClick={() => setMicrobiotaOpen(!microbiotaOpen)}
         >
-          microbiota{' '}
+          (ver qué es)
         </span>
-        intestinal, que son clave y que están muy
-        relacionadas con el estado de salud de cualquier
-        persona
+        , que son clave y que están muy relacionadas con el
+        estado de salud de cualquier persona
       </p>
-      <div className='me_areas'>
-        <div className='me_areas_area'>
-          <p
-            className='me_areas_area-title'
-            onClick={() => setPnieOpen(!pnieOpen)}
-          >
-            La Psiconeuroinmunoendocrinología (PNIE):
-            {/* <span className='me_areas_area-title-svg'>
-              <Dropdown />
-            </span> */}
-          </p>
-          {pnieOpen && (
-            <>
-              <p className='me_areas_area-text'>
-                Es la ciencia que estudia la comunicación
-                entre nuestros procesos psicológicos y los
-                sistemas que mantienen el equilibrio nuestro
-                organismo: el sistema nervioso, el endocrino
-                y el inmune. De este campo nacen las
-                Terapias Mente-Cuerpo, intervenciones que
-                emplean diferentes técnicas para potenciar
-                la capacidad de la mente para afectar la
-                función corporal. <br />
-                Esta rama de la salud está estrechamente
-                relacionada con otra de las áreas en las que
-                decidí formarme
-              </p>
-              <img src={PNIE} alt='PNIE ciclo' />
-            </>
-          )}
+      {(pnieOpen || microbiotaOpen) && (
+        <div className='me_areas'>
+          <div className='me_areas_area'>
+            {pnieOpen && (
+              <>
+                <p
+                  className='me_areas_area-title'
+                  onClick={() => setPnieOpen(!pnieOpen)}
+                >
+                  La Psiconeuroinmunoendocrinología (PNIE):
+                </p>
+                <img src={PNIE} alt='PNIE ciclo' />
+                <p className='me_areas_area-text'>
+                  Es la ciencia que estudia la comunicación
+                  entre nuestros procesos psicológicos y los
+                  sistemas que mantienen el equilibrio
+                  nuestro organismo: el sistema nervioso, el
+                  endocrino y el inmune. De este campo nacen
+                  las Terapias Mente-Cuerpo, intervenciones
+                  que emplean diferentes técnicas para
+                  potenciar la capacidad de la mente para
+                  afectar la función corporal. <br />
+                  Esta rama de la salud está estrechamente
+                  relacionada con otra de las áreas en las
+                  que decidí formarme
+                </p>
+              </>
+            )}
+          </div>
+          <div className='me_areas_area'>
+            {microbiotaOpen && (
+              <>
+                <p
+                  className='me_areas_area-title'
+                  onClick={() =>
+                    setMicrobiotaOpen(!microbiotaOpen)
+                  }
+                >
+                  La Microbiota intestinal:
+                </p>
+
+                <img src={microbiota} alt='PNIE ciclo' />
+                <p className='me_areas_area-text'>
+                  La microbiota intestinal desempeña un
+                  papel crucial en nuestra salud y
+                  bienestar, ya que además de influir en la
+                  función digestiva y la absorción de
+                  nutrientes, también está en comunicación
+                  con el sistema inmunológico, influye en el
+                  metabolismo y en la producción de
+                  neurotransmisores siendo un factor clave
+                  en el estado de salud físico, mental y
+                  emocional, pilares fundamentales del
+                  bienestar de todo ser humano.
+                </p>
+              </>
+            )}
+          </div>
         </div>
-        <div className='me_areas_area'>
-          <p
-            className='me_areas_area-title'
-            onClick={() =>
-              setMicrobiotaOpen(!microbiotaOpen)
-            }
-          >
-            La Microbiota intestinal:
-            {/* <span className='me_areas_area-title-svg'>
-              <Dropdown />
-            </span> */}
-          </p>
-          {microbiotaOpen && (
-            <>
-              <p className='me_areas_area-text'>
-                La microbiota intestinal desempeña un papel
-                crucial en nuestra salud y bienestar, ya que
-                además de influir en la función digestiva y
-                la absorción de nutrientes, también está en
-                comunicación con el sistema inmunológico,
-                influye en el metabolismo y en la producción
-                de neurotransmisores siendo un factor clave
-                en el estado de salud físico, mental y
-                emocional, pilares fundamentales del
-                bienestar de todo ser humano.
-              </p>
-              <img src={microbiota} alt='PNIE ciclo' />
-            </>
-          )}
-        </div>
-      </div>
+      )}
       <p>
         El combinar e integrar la nutrición con estas áreas
         nos da la posibilidad de abordar la salud de una
-        manera más holística apoyándonos en una alimentación
-        y estilo de vida consiente. Consciente con nosotros
-        mismos y con nuestro entorno. Este es el enfoque con
-        el que me gustar brindar mis servicios y que en
-        Nutrición se lo conoce como:
+        manera<b> holística </b>apoyándonos en una
+        alimentación y estilo de vida<b> consciente</b>. Consciente
+        con nosotros mismos y con nuestro entorno. Este es
+        el enfoque con el que me gustar brindar mis
+        servicios y que en Nutrición se lo conoce como:
       </p>
     </div>
   )
